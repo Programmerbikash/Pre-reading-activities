7d801e09bd68215cc467d66c36e13bae695e2fa1
Programmerbikash <bikash19081998dey@gmail.com>
2022-09-29 07:57:19 +0600

    pofile image complete

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import image from '../Images/bikash.jpg';
 
 const Sidebar = () => {
     return (
-        <div>
-            <h1>Hello sidebar</h1>
+        <div className='sidebar'>
+            <div className="profile">
+                <img src={image} alt="" />
+                <div>
+                    <p className='profile-name'>Bikash Chandra Dey</p>
+                    <p><FontAwesomeIcon icon={faLocationDot} /> <small>Cox's Bazar, Bangladesh</small></p>
+                </div>
+            </div>
+            <div>
+                <p>Weight: </p>
+                <p>Height: </p>
+                <p>Age: </p>
+            </div>
         </div>
     );
 };
